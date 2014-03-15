7068ce68e94282596ae47a1f6e69e52b00a9ccd7
Parth <parth.v.thakkar@gmail.com>
2014-03-16 00:56:55 +0100

    BaseController

---
+++ JSMT/src/main/java/org/slf/controllers/BaseController.java
@@ -0,0 +1,25 @@
+package org.slf.controllers;
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@EnableAutoConfiguration
+public class BaseController {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BaseController.class, args);
+    }
+	
+	
+}
